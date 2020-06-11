afca80241536871b9184b400ed5e315be81c0ee4
TuffyTian <tiantengfei@outlook.com>
2020-06-11 11:47:10 +0900

    init project and add currencyHomeItemView

---
+++ ModernCurrency/View/CurrencyHomeItemView.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyHomeItemView.swift
+//  ModernCurrency
+//
+//  Created by TuffyTian on 2020/6/11.
+//  Copyright © 2020 Tengfei Tian. All rights reserved.
+//
+
+import SwiftUI
+
+struct CurrencyHomeItemView: View {
+    @Binding var textValue: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Amercian USD")
+                .font(.system(size: 12))
+                .foregroundColor(.gray)
+            HStack {
+                Text("USD")
+                    .font(.system(size: 20))
+                Spacer()
+                TextField("0", text: $textValue)
+                    .font(.system(size: 20))
+                    .padding(.leading, 10)
+            }
+        }
+    }
+}
+
+struct CurrencyHomeItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrencyHomeItemView(textValue: .constant("123"))
+    }
+}
